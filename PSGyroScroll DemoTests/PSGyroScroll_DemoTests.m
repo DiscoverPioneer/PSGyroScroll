c5088197cf6378b8b6cce0d2f2858bf006b56b19
DiscoverPioneer <philscarfi@PhilScarfi-MBP.local>
2014-08-12 16:19:44 -0400

    Initial Commit

---
+++ PSGyroScroll DemoTests/PSGyroScroll_DemoTests.m
@@ -0,0 +1,34 @@
+//
+//  PSGyroScroll_DemoTests.m
+//  PSGyroScroll DemoTests
+//
+//  Created by Phil Scarfi on 8/12/14.
+//  Copyright (c) 2014 BuzzFeed Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PSGyroScroll_DemoTests : XCTestCase
+
+@end
+
+@implementation PSGyroScroll_DemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
